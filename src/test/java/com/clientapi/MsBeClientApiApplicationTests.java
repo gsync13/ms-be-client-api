4ebc824e6fd9fabd219fa183ab43086cf2e208f9
Gonçalo Meireles <gongax.m@gmail.com>
2024-04-26 18:19:20 +0100

    init

---
+++ src/test/java/com/clientapi/MsBeClientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBeClientApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
